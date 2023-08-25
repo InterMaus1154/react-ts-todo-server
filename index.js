97bcf0599aed0aa41e53d63eee2aa2d6d2df51f9
InterMaus1154 <amarco1154@gmail.com>
2023-08-25 09:50:39 +0100

    Working login and register system

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ app.use(cors());
 const http = require('http');
 const httpServer = http.createServer(app);
 
-
+const fs = require('fs');
+const User = require('./User.js')
 
 const {Server} = require("socket.io");
+
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 
 const io = new Server(httpServer, {
     cors:{
@@ -19,4 +23,67 @@ const io = new Server(httpServer, {
 
 httpServer.listen(3001, ()=>{
     console.log("Server is running")
+
+});
+
+io.on("connection", socket=>{
+    console.log("Connection established");
+
+    socket.on("user_is_guest", data =>{
+        console.log(socket.id + " is logged in as guest "+data.isGuest);
+    });
+
+    socket.on("request_username_check", data =>{
+        const userList = JSON.parse(fs.readFileSync("./UserList.json", "utf8"));
+        socket.emit("username_check_response", {isUserExist: userList.includes(data.user.username.trim().toLowerCase()), user: data.user});
+    });
+
+    socket.off("register_user", ()=>{}).on("register_user", data =>{
+        const existingUsersList = JSON.parse(fs.readFileSync("./UserList.json"));
+        const newUsersList = [...existingUsersList, data.user.username.trim().toLowerCase()];
+
+        const hash = bcrypt.hashSync(data.password, 10);
+        const user = {
+            userID: crypto.randomUUID(),
+            userName: data.user.username,
+            displayName: data.user.userDisplayName,
+            password: hash
+        };
+
+
+        const existingUsersObject = JSON.parse(fs.readFileSync("./Users.json"));
+        const newUsersObject = {...existingUsersObject, [user.userName]: user};
+
+        fs.writeFileSync("./Users.json", JSON.stringify(newUsersObject, null, 2));
+        fs.writeFileSync("./UserList.json", JSON.stringify(newUsersList));
+
+        console.log(data.user.username+" is registered");
+
+        socket.emit("user_registered", {isUserRegistered: true});
+    });
+
+
+    socket.off("request_user_auth", ()=>{}).on("request_user_auth", data =>{
+        
+        const username = data.username.toLowerCase();
+        const existingUsersList = JSON.parse(fs.readFileSync("./UserList.json", "utf-8"));
+
+        const isUserExist = existingUsersList.includes(username);
+        
+        if(isUserExist){
+            const existingUsers = JSON.parse(fs.readFileSync("./Users.json", "utf-8"));
+            const hashedPassword = existingUsers[username].password;
+            const user = {
+                username: existingUsers[username].userName,
+                displayname: existingUsers[username].displayName
+            };
+            const isUserAuthorised = bcrypt.compareSync(data.password, hashedPassword);
+            socket.emit("user_auth_response", {isUserExist: existingUsersList.includes(username), isUserAuthorised: isUserAuthorised, user: user});
+        }else{
+            socket.emit("user_auth_response", {isUserExist: existingUsersList.includes(username)});
+        }
+        
+    
+    }); 
+
 });
