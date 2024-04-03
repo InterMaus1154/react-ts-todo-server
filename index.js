518e27e4e49d344fbc252d79d435f10e6ff19871
InterMaus1154 <114827364+InterMaus1154@users.noreply.github.com>
2024-04-03 09:21:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const bcrypt = require('bcrypt');
 
 const io = new Server(httpServer, {
     cors:{
-        origin: "https://verinatodoappbeta.onrender.com/",
+        origin: "https://verinatodoappbeta.onrender.com",
         methods: ['GET', 'POST']
     }
 });
